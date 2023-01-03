9140ba58b54fe4192d783ec7e5093719fa08bb3c
lauromiottojr <junior.miotto36@gmail.com>
2023-01-03 19:15:08 -0300

    Creating project

---
+++ src/main/java/com/testjavaattornatus/TestJavaAttornatusApplication.java
@@ -0,0 +1,13 @@
+package com.testjavaattornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJavaAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJavaAttornatusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testjavaattornatus/TestJavaAttornatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testjavaattornatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJavaAttornatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
